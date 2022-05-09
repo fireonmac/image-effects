4c2bda7958ca20e365a0080b81a84db7896a9e3c
Paul NOH <fireonmac@gmail.com>
2022-05-10 08:46:47 +0900

    logging file

--- public/main.js
+++ public/main.js
@@ -8,16 +8,13 @@ async function init() {
     return;
   }
 
-  console.log(rustApp);
-
   const input = document.getElementById('upload');  
   const fileReader = new FileReader();
 
   fileReader.onloadend = () => {
     const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
 
-    console.log(input.files[0]);
-    console.log(base64);
+    rustApp.grayscale(base64);
   };
 
 
