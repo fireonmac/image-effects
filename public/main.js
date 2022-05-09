6ea631e434c4b100a1924051af102999a0d76289
Paul NOH <fireonmac@gmail.com>
2022-05-09 20:13:03 +0900

    import wasm

--- public/main.js
+++ public/main.js
@@ -1,13 +1,26 @@
-function init() {
+async function init() {
+  let rustApp = null;
+
+  try {
+    rustApp = await import('../pkg')
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  console.log(rustApp);
+
   const input = document.getElementById('upload');  
   const fileReader = new FileReader();
 
   fileReader.onloadend = () => {
     const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
-    
+
     console.log(input.files[0]);
     console.log(base64);
   };
+
+
 
   input.addEventListener('change', () => {
     fileReader.readAsDataURL(input.files[0]);
